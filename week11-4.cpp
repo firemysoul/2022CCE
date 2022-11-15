152f0cf1922af2408d9ebe3e82395390b6248438
firemysoul <phoenix20211024@gmail.com>
2022-11-15 12:17:44 +0800

    week11

---
+++ week11-4.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+int main()
+{
+    int a[10]={0, 80, 70, 60, 50, 40, 30, 20, 10, 90};
+
+
+    for(int i = 0; i<10-1; i++)
+    {
+        if( a[i] > a[i-1] )
+        {
+            int temp = a[i];
+            a[i] = a[i+1];
+            a[i+1] =temp;
+        }
+    }
+    for(int i = 0; i < 10; i++)
+    printf("%d ", a[i] );
+    printf("\n");
+    for(int i = 0; i<10-1; i++)
+    {
+        if( a[i] > a[i-1] )
+        {
+            int temp = a[i];
+            a[i] = a[i+1];
+            a[i+1] =temp;
+        }
+    }
+    for(int i = 0; i < 10; i++)
+    printf("%d ", a[i] );
+    printf("\n");
+    for(int i = 0; i<10-1; i++)
+    {
+        if( a[i] > a[i-1] )
+        {
+            int temp = a[i];
+            a[i] = a[i+1];
+            a[i+1] =temp;
+        }
+    }
+    for(int i = 0; i < 10; i++)
+    printf("%d ", a[i] );
+    printf("\n");
+    for(int i = 0; i<10-1; i++)
+    {
+        if( a[i] > a[i-1] )
+        {
+            int temp = a[i];
+            a[i] = a[i+1];
+            a[i+1] =temp;
+        }
+    }
+    for(int i = 0; i < 10; i++)
+    printf("%d ", a[i] );
+    printf("\n");
+}
