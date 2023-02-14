4147b01d638dae059f38959ffcc68e4c93cd417c
firemysoul <phoenix20211024@gmail.com>
2023-02-14 11:59:19 +0800

    整理舊程式、加week01

--- week01/week01-2.cpp
+++ week01/week01-2.cpp
@@ -1,10 +1,1 @@
-#include <stdio.h>
 
-int main()
-{
- printf ("Hello World\n");
- printf ("Hello World\n");
- printf ("我是曾文豪\n");
- //不知道打什麼
- ///3條深一點
- }
---
+++ week01/week01-5.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char line[30];
+	int total=0;
+		int N =strlen(line);
+
+		int ans=0;
+		for(int i=0; i<N; i++)
+		{
+			if( line[i]=='2') ans++;
+		}
+		printf("%d\n",ans);
+		total += ans;
+	printf("Total: %d\n",total);
+}
