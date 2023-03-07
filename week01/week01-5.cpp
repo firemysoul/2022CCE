3eff5ff8bbd0499d90b21abedc34e3ca6efef1f0
firemysoul <112919873+firemysoul@users.noreply.github.com>
2023-03-07 10:07:51 +0800

    Update week01-5.cpp

--- week01/week01-5.cpp
+++ week01/week01-5.cpp
@@ -4,14 +4,14 @@ int main()
 {
 	char line[30];
 	int total=0;
-		int N =strlen(line);
+	int N =strlen(line);
 
-		int ans=0;
-		for(int i=0; i<N; i++)
-		{
-			if( line[i]=='2') ans++;
-		}
-		printf("%d\n",ans);
-		total += ans;
+	int ans=0;
+	for(int i=0; i<N; i++)
+	{
+		if( line[i]=='2') ans++;
+	}
+	printf("%d\n",ans);
+	total += ans;
 	printf("Total: %d\n",total);
 }
