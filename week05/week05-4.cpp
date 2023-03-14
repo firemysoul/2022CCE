900f69cbb0faab923820cd043057398e164530c5
firemysoul <phoenix20211024@gmail.com>
2023-03-14 12:10:58 +0800

    week05

---
+++ week05/week05-4.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int used[26] = {};
+
+    char c;
+    while( scanf("%c", &c)==1)
+    {
+        int i;
+        if(c>='A' && c<='Z') i= c - 'A';
+        if(c>='a' && c<='z') i= c - 'a';
+        else if(c ==' ') i= 0;
+        used[i] ++;
+    }
+    int bad=0;
+    for(int i=0; i<26; i++)
+    {
+        if(used[i]==0) bad=1;
+    }
+    if (bad==0) printf("Yes");
+    else printf("No");
+}
+
